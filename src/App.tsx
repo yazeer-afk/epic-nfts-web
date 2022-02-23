7c8a30771059ea4e34f4ef59dc4118ff309d5b50
yazeer-afk <int.yazeer@gmail.com>
2022-02-23 22:38:22 +0530

    - Removed unwanted files - Created the boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Footer } from './components/Footer.component'
 
 function App() {
+
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.component.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import { TwitterLogo } from './TwitterLogo.component'
+const twitterLogo = require('../assets/twitter-logo.svg');
+
+export interface FooterProps {
+
+}
+
+export const Footer: FC<FooterProps> = (props) => {
+
+    const TWITTER_HANDLE = '_buildspace';
+    const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+    const OPENSEA_LINK = '';
+    const TOTAL_MINT_COUNT = 50;
+
+    return (
+        <div className="footer-container">
+            {/* <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} /> */}
+            <TwitterLogo />
+            <a
+                className="footer-text"
+                href={TWITTER_LINK}
+                target="_blank"
+                rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+    )
+}
