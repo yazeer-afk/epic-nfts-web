00e40441dad6340ce850460cacd9b61cd316b0f9
yazeer-afk <int.yazeer@gmail.com>
2022-03-03 00:18:43 +0530

    - Removed unused variables

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -9,8 +9,8 @@ export const Footer: FC<FooterProps> = (props) => {
 
     const TWITTER_HANDLE = '_buildspace';
     const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-    const OPENSEA_LINK = '';
-    const TOTAL_MINT_COUNT = 50;
+    // const OPENSEA_LINK = '';
+    // const TOTAL_MINT_COUNT = 50;
 
     return (
         <div className="footer-container">
